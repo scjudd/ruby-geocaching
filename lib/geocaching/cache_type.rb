d693038766fa98c90d53897ec8bf0ecf5530924d
Thomas Cyron <thomas@thcyron.de>
2010-08-12 15:07:11 +0200

    Update cache type names to official ones

--- lib/geocaching/cache_type.rb
+++ lib/geocaching/cache_type.rb
@@ -1,20 +1,20 @@
 module Geocaching
   class CacheType
     TYPES = {
-      :traditional  => [2, "Traditional Cache"],
-      :multi        => [3, "Multi Cache"],
-      :mystery      => [8, "Mystery Cache"],
-      :letterbox    => [5, "Letterbox Hybrid"],
-      :wherigo      => [1858, "Wherigo"],
-      :event        => [6, "Event Cache"],
-      :megaevent    => [453, "Mega Event Cache"],
-      :cito         => [13, "Cache In Trash Out Event"],
-      :earthcache   => [137, "EarthCache"],
+      :traditional  => [2,    "Traditional Cache"],
+      :multi        => [3,    "Multi-cache"],
+      :mystery      => [8,    "Unknown Cache"],
+      :letterbox    => [5,    "Letterbox Hybrid"],
+      :wherigo      => [1858, "Wherigo Cache"],
+      :event        => [6,    "Event Cache"],
+      :megaevent    => [453,  "Mega-Event Cache"],
+      :cito         => [13,   "Cache In Trash Out Event"],
+      :earthcache   => [137,  "Earthcache"],
       :lfevent      => [3653, "Lost and Found Event Cache"],
-      :locationless => [12, "Locationless Reverse Cache"],
-      :webcam       => [11, "Webcam Cache"],
-      :virtual      => [4, "Virtual Cache"],
-      :ape          => [32, "Project A.P.E. Cache"] # XXX
+      :locationless => [12,   "Locationless (Reverse) Cache"],
+      :webcam       => [11,   "Webcam Cache"],
+      :virtual      => [4,    "Virtual Cache"],
+      :ape          => [9,    "Project APE Cache"]
     }
 
     def self.for_id(id)
