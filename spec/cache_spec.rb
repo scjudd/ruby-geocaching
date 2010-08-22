b4d1b862b16cfdbfa4a0ee6ed15492d3e00ecf0d
Thomas Cyron <thomas@thcyron.de>
2010-08-22 19:47:28 +0200

    "." isn't in $LOAD_PATH anymore on Ruby 1.9.2

--- spec/cache_spec.rb
+++ spec/cache_spec.rb
@@ -1,10 +1,11 @@
 # encoding: utf-8
 
 $:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+dir = File.dirname(__FILE__)
 
 require "geocaching"
-require "helper"
+require "#{dir}/helper"
 
 Geocaching::CacheType::TYPES.to_a.map { |a| a[0].to_s }.each do |type|
-  require "cache/#{type}"
+  require "#{dir}/cache/#{type}"
 end
--- spec/log_spec.rb
+++ spec/log_spec.rb
@@ -1,13 +1,14 @@
 # encoding: utf-8
 
 $:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+dir = File.dirname(__FILE__)
 
 require "geocaching"
-require "helper"
+require "#{dir}/helper"
 
 Geocaching::LogType::TYPES.to_a.map { |a| a[0].to_s }.each do |type|
   begin
-    require "log/#{type}"
+    require "#{dir}/log/#{type}"
   rescue LoadError
     $stderr.puts "Missing test for log type #{type}"
   end
