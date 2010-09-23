bceb6ce7aced1d4d519481fa7b8c137fbd324431
Thomas Cyron <thomas@thcyron.de>
2010-09-23 21:08:53 +0200

    Use another log in ‘Disable Listing’ log test

--- spec/log/disable.rb
+++ spec/log/disable.rb
@@ -1,16 +1,16 @@
 # encoding: utf-8
 
-describe "Geocaching::Log for 1758e5da-b49f-4a0a-b435-d12cd8d66e86 (Disable Listing)" do
+describe "Geocaching::Log for 83a3f4f4-7faf-414a-8145-8f334622d20f (Disable Listing)" do
   before :all do
-    @log = Geocaching::Log.fetch(:guid => "1758e5da-b49f-4a0a-b435-d12cd8d66e86")
+    @log = Geocaching::Log.fetch(:guid => "83a3f4f4-7faf-414a-8145-8f334622d20f")
   end
 
   it "should return the correct username" do
-    @log.username.should == "Cermak"
+    @log.username.should == "CacheHasen"
   end
 
   it "should return the correct cache GUID" do
-    @log.cache.guid.should == "66274935-40d5-43d8-8cc3-c819e38f9dcc"
+    @log.cache.guid.should == "65f58a1c-916b-4b32-a656-e80964b6819b"
   end
 
   it "should return the correct type" do
@@ -18,7 +18,7 @@ describe "Geocaching::Log for 1758e5da-b49f-4a0a-b435-d12cd8d66e86 (Disable List
   end
 
   it "should return the correct date" do
-    @log.date.should == Time.mktime(2010, 2, 27)
+    @log.date.should == Time.mktime(2010, 3, 13)
   end
 
   it "should return the correct message" do
