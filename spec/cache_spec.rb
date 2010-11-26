767627bc69d982e211dd4f7801a47331b39aae9e
Thomas Cyron <thomas@thcyron.de>
2010-11-26 21:29:45 +0100

    Allow to specify cache types to test with env var

--- spec/cache_spec.rb
+++ spec/cache_spec.rb
@@ -6,7 +6,13 @@ dir = File.dirname(__FILE__)
 require "geocaching"
 require "#{dir}/helper"
 
-Geocaching::CacheType::TYPES.to_a.map { |a| a[0].to_s }.each do |type|
+if ENV["GC_CACHE_TYPES"]
+  types = ENV["GC_CACHE_TYPES"].split
+else
+  types = Geocaching::CacheType::TYPES.to_a.map { |a| a[0].to_s }
+end
+
+types.each do |type|
   # Locationless caches have completely been disabled on geocaching.com
   next if type == "locationless"
 
